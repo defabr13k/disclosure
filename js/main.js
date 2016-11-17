ecd8bd8588356e0d0fd12a9d4df8f858f8388a15
drdynscript <drdynscript@gmail.com>
2016-11-17 16:50:47 +0100

    Update disclosure with slide dimension calculations

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function ready(cb) {
+    /in/.test(document.readyState)
+    ? setTimeout(ready.bind(null, cb), 90)
+    : cb();
+};
+
+ready(function(){
+
+    var App = {
+        "init": function() {
+          this._slideRatio = SlideRatios.major_seventh;
+
+          this.calculateDimensionsSlides();
+          this.registerEventListeners();
+        },
+        "registerEventListeners": function() {
+          var self = this;
+
+          window.addEventListener('resize', function(ev) {
+            self.calculateDimensionsSlides();
+          });
+
+        },
+        "calculateDimensionsSlides": function() {
+          var slidesElement = document.querySelector('.dc-slides');
+          var slideMaxWidth = slidesElement.offsetWidth;
+          var slideMaxHeight = slidesElement.offsetHeight;
+          var slideWidth = slideMaxWidth;
+          var slideHeight = this._slideRatio*slideMaxWidth;
+          if(slideHeight > slideMaxHeight) {
+            slideHeight = slideMaxHeight;
+            slideWidth = slideHeight/this._slideRatio;
+          }
+
+          var slides = document.querySelectorAll('.dc-slide');
+          slides.forEach(function(slide) {
+            slide.style.width = slideWidth + 'px';
+            slide.style.left = (slideMaxWidth - slideWidth) / 2 + 'px';
+            slide.style.height = slideHeight + 'px';
+            slide.style.top = (slideMaxHeight - slideHeight) / 2 + 'px';
+          });
+
+        }
+    };
+
+    App.init();
+    
+});
---
+++ js/models.js
@@ -0,0 +1,18 @@
+var SlideRatios = {
+  'minor_second'  : 15/16,
+	'major_second'  : 8/9,
+	'minor_third'   : 5/6,
+	'major_third'   : 4/5,
+  'perfect_fourth': 3/4,
+  'perfect_fifth' : 2/3,
+  'minor_sixth'   : 5/8,
+  'golden_section': 1/1.618,
+  'major_sixth'   : 3/5,
+  'minor_seventh' : 9/16,
+  'major_seventh' : 8/15,
+  'octave'        : 1/2,
+  'major_tenth'   : 2/5,
+  'major_eleventh': 3/8,
+  'major_twelfth' : 1/3,
+  'octave_double' : 1/4
+};
